5b7149ffa9c3139cd1e2236b6aaebf7cda6e537e
bouajilaProg <bouajilamedyessine@gmail.com>
2024-11-11 12:25:22 +0100

    rust now able to deliver

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
 use std::io::{self, Read};
+use std::path::Path;
+
+const REPO_PATH: &str = "./repo";
 
 fn read_file_contents(file_path: &str) -> io::Result<String> {
     let mut file = File::open(file_path)?;
@@ -8,10 +11,24 @@ fn read_file_contents(file_path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn main() -> io::Result<()> {
-    let filename = "./test.xml";
+#[tauri::command]
+fn fetch_doc(lang: String, category: String, name: String) -> String {
+    let file_path = format!("{}/{}/{}/{}.xml", REPO_PATH, lang, category, name);
 
-    println!("Reading file: {}", filename);
+    if Path::new(&file_path).exists() {
+        match read_file_contents(&file_path) {
+            Ok(contents) => contents,
+            Err(e) => format!("Error reading file: {}", e),
+        }
+    } else {
+        "File not found".to_string()
+    }
+}
 
-    Ok(())
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![fetch_doc])
+        .run(tauri::generate_context!())
+        .expect("error while running Tauri application");
 }
